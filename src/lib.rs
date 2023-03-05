92d601bfc548e33379a99dc3c1fb4da616ceda9b
tobi <tobias.larsson96@gmail.com>
2023-03-05 22:31:59 +0200

    Instancing implemented

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use camera_controller::CameraController;
 //use wgpu::RenderPipeline;
 use wgpu::util::DeviceExt;
 use winit::window::Window;
-
+use cgmath::prelude::*;
 
 mod texture;
 mod camera_controller;
@@ -88,7 +88,7 @@ impl Camera {
 struct CameraUniform {
 	// We can't use cgmath with bytemuck directly so we'll have
     // to convert the Matrix4 into a 4x4 f32 array
-	view_proj: [[f32; 4]; 4],
+    view_proj: [[f32; 4]; 4],
 }
 
 impl CameraUniform {
@@ -101,6 +101,63 @@ impl CameraUniform {
 
 	fn update_view_proj(&mut self, camera: &Camera) {
 		self.view_proj = camera.build_view_projection_matrix().into();
+	}
+}
+
+struct Instance {
+	position: cgmath::Vector3<f32>,
+	rotation: cgmath::Quaternion<f32>,
+}
+
+impl Instance {
+	fn to_raw(&self) -> InstanceRaw {
+		InstanceRaw {
+			model: (cgmath::Matrix4::from_translation(self.position) * cgmath::Matrix4::from(self.rotation)).into(),
+		}
+	}
+}
+#[repr(C)]
+#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct InstanceRaw {
+	model: [[f32; 4]; 4],
+}
+
+impl InstanceRaw {
+	fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+		use std::mem;
+		wgpu::VertexBufferLayout {
+			array_stride: mem::size_of::<InstanceRaw>() as wgpu::BufferAddress,            // We need to switch from using a step mode of Vertex to Instance
+            // This means that our shaders will only change to use the next
+            // instance when the shader starts processing a new instance
+			step_mode: wgpu::VertexStepMode::Instance,
+			attributes: &[
+				wgpu::VertexAttribute {
+					offset: 0,
+                    // While our vertex shader only uses locations 0, and 1 now, in later tutorials we'll
+                    // be using 2, 3, and 4, for Vertex. We'll start at slot 5 not conflict with them later
+					shader_location: 5,
+					format: wgpu::VertexFormat::Float32x4,
+				},
+                // A mat4 takes up 4 vertex slots as it is technically 4 vec4s. We need to define a slot
+                // for each vec4. We'll have to reassemble the mat4 in
+                // the shader.
+				wgpu::VertexAttribute {
+					offset: mem::size_of::<[f32; 4]>() as wgpu::BufferAddress,
+					shader_location: 6,
+					format: wgpu::VertexFormat::Float32x4,
+				},
+				wgpu::VertexAttribute {
+					offset: mem::size_of::<[f32; 8]>() as wgpu::BufferAddress,
+					shader_location: 7,
+					format: wgpu::VertexFormat::Float32x4,
+				},
+				wgpu::VertexAttribute {
+					offset: mem::size_of::<[f32; 12]>() as wgpu::BufferAddress,
+					shader_location: 8,
+					format: wgpu::VertexFormat::Float32x4,
+				},
+			]
+		}
 	}
 }
 
@@ -123,6 +180,8 @@ struct State {
 	camera_buffer: wgpu::Buffer,
 	camera_bind_group: wgpu::BindGroup,
 	camera_controller: camera_controller::CameraController,
+	instances: Vec<Instance>,
+	instance_buffer: wgpu::Buffer,
 }
 
 impl State {
@@ -130,6 +189,7 @@ impl State {
     // Creating some of the wgpu types requires async code
     async fn new(window: Window) -> Self {
         let size = window.inner_size();
+
         // The instance is a handle to our GPU
         // Backends::all => Vulkan + Metal + DX12 + Browser WebGPU
         let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
@@ -290,6 +350,34 @@ impl State {
 
 		let camera_controller = CameraController::new(0.2);
 
+		const NUM_INSTANCES_PER_ROW: u32 = 10;
+		const INSTANCE_DISPLACEMENT: cgmath::Vector3<f32> = cgmath::Vector3::new(NUM_INSTANCES_PER_ROW as f32 * 0.5, 0.0, NUM_INSTANCES_PER_ROW as f32 * 0.5);
+
+		let instances = (0..NUM_INSTANCES_PER_ROW).flat_map(|z| {
+			(0..NUM_INSTANCES_PER_ROW).map(move |x| {
+				let position = cgmath::Vector3 { x: x as f32, y: 0.0, z: z as f32} - INSTANCE_DISPLACEMENT;
+				
+				let rotation = if position.is_zero() {
+					cgmath::Quaternion::from_axis_angle(cgmath::Vector3::unit_z(),cgmath::Deg(0.0))
+				} else {
+					cgmath::Quaternion::from_axis_angle( position.normalize(),cgmath::Deg(45.0))
+				};
+
+				Instance {
+					position, rotation,
+				}
+			})
+		}).collect::<Vec<_>>();
+
+		let instance_data = instances.iter().map(Instance::to_raw).collect::<Vec<_>>();
+		let instance_buffer = device.create_buffer_init(
+			&wgpu::util::BufferInitDescriptor {
+				label: Some("Instance Buffer"),
+				contents: bytemuck::cast_slice(&instance_data),
+				usage: wgpu::BufferUsages::VERTEX,
+			}
+		);
+
         let bg_color = wgpu::Color {
 			r: 0.005,
 			g: 0.005,
@@ -320,6 +408,7 @@ impl State {
 				entry_point: "vs_main",					// vertex shader entrypoint
 				buffers: &[
 					Vertex::desc(),
+					InstanceRaw::desc(),
 				],							// vertex types?
 			},
 			fragment: Some(wgpu::FragmentState {		//optional?
@@ -388,6 +477,8 @@ impl State {
 			camera_buffer,
 			camera_bind_group,
 			camera_controller,
+			instances,
+			instance_buffer,
         }
     }
 
@@ -456,12 +547,14 @@ impl State {
 			
 			render_pass.set_pipeline(&self.render_pipeline);
 			render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
-			
 			render_pass.set_bind_group(1, &self.camera_bind_group, &[]);
+
 			render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+			render_pass.set_vertex_buffer(1, self.instance_buffer.slice(..));
+			
 			render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
 
-			render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
+			render_pass.draw_indexed(0..self.num_indices, 0, 0..self.instances.len() as _);
 
         }
         
