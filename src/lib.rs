3966548c7008d19f49185024f8a556a98e222ba3
tobi <tobias.larsson96@gmail.com>
2023-03-08 00:19:52 +0200

    nonworking state

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,14 @@
-use camera_controller::CameraController;
-//use wgpu::RenderPipeline;
 use wgpu::util::DeviceExt;
 use winit::window::Window;
 use cgmath::prelude::*;
 
-
-
 mod resources;
 mod texture;
 mod model;
-mod camera_controller;
+mod camera;
 
 use model::{Vertex, DrawModel};
+use camera::{Camera, CameraController};
 
 // lib.rs
 
@@ -23,46 +20,8 @@ pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
 	0.0, 0.0, 0.5, 1.0,
 );
 
-struct Camera {
-	eye: cgmath::Point3<f32>,
-	target: cgmath::Point3<f32>,
-	up: cgmath::Vector3<f32>,
-	aspect: f32,
-	fovy: f32,
-	znear: f32,
-	zfar: f32,
-}
 
-impl Camera {
 
-	fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
-		let view = cgmath::Matrix4::look_at_rh(self.eye, self.target, self.up);
-		let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
-		
-		OPENGL_TO_WGPU_MATRIX * proj * view
-	}
-}
-// We need this for Rust to store our data correctly for the shaders
-#[repr(C)]
-// This is so we can store this in a buffer
-#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
-struct CameraUniform {
-	// We can't use cgmath with bytemuck directly so we'll have
-    // to convert the Matrix4 into a 4x4 f32 array
-    view_proj: [[f32; 4]; 4],
-}
-
-impl CameraUniform {
-	fn new() -> Self {
-		Self {
-			view_proj: cgmath::Matrix4::identity().into(),
-		}
-	}
-
-	fn update_view_proj(&mut self, camera: &Camera) {
-		self.view_proj = camera.build_view_projection_matrix().into();
-	}
-}
 
 struct Instance {
 	position: cgmath::Vector3<f32>,
